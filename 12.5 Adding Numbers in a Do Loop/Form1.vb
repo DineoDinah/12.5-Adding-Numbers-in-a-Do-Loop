cfd0fe33935607f8cf5cec77b9f5ad5eae8d04bb
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-05 22:28:13 +0200

    Add project files.

---
+++ 12.5 Adding Numbers in a Do Loop/Form1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Diagnostics.Metrics
+
+Public Class Form1
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Dim Counter As Integer
+		Do While Counter <= 100
+			Counter += 1
+
+			ListBox1.Items.Add(Counter)
+
+
+		Loop
+	End Sub
+End Class
